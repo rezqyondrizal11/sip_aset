a9de4e07915985bf1501c56452e77f9e6fd5b9b1
rezqyondrizal11 <67118023+rezqyondrizal11@users.noreply.github.com>
2024-12-16 00:50:03 +0700

    update

--- app/Http/Controllers/AsetTetapController.php
+++ app/Http/Controllers/AsetTetapController.php
@@ -637,24 +637,62 @@ class AsetTetapController extends Controller
         }
 
 
+        switch ($categoryId) {
+            case 1:
+                return $this->datawali1($query);
+            case 2:
+                return $this->datawali2($query);
+            case 3:
+                return $this->datawali3($query);
+            case 4:
+                return $this->datawali4($query);
+            case 5:
+                return $this->datawali5($query);
+            default:
+                return response()->json(['message' => 'Kategori tidak ditemukan'], 404);
+        }
+    }
+    private function datawali1($query)
+    {
         return DataTables::of($query)
             ->addIndexColumn()
 
-
             ->addColumn('nama_barang', function ($row) {
-                $name = $row->jenisaset->name  . ' / ' .  $row->nama_barang;
-                return $name;
+                return $row->jenisAset->name . ' / ' . $row->nama_barang;
             })
             ->addColumn('sertifikat_tgl', function ($row) {
-                if ($row->sertifikat_tgl) {
-                    return date('d-M-Y', strtotime($row->sertifikat_tgl));
-                }
-                return '-';
-            })->addColumn('tanggal_perolehan', function ($row) {
-                if ($row->tanggal_perolehan) {
-                    return date('d-M-Y', strtotime($row->tanggal_perolehan));
-                }
-                return '-';
+                return $row->kibATanah && $row->kibATanah->sertifikat_tgl
+                    ? date('d-M-Y', strtotime($row->kibATanah->sertifikat_tgl))
+                    : '-';
+            })
+            ->addColumn('luas', function ($row) {
+                return $row->kibATanah->luas ?? '-'; // Jika relasi kosong, tampilkan "-"
+            })
+            ->addColumn('tahun_pengadaan', function ($row) {
+                return $row->kibATanah->tahun_pengadaan ?? '-';
+            })
+            ->addColumn('alamat', function ($row) {
+                return $row->kibATanah->alamat ?? '-';
+            })
+            ->addColumn('status_tanah', function ($row) {
+                return $row->kibATanah->status_tanah ?? '-';
+            })
+            ->addColumn('sertifikat_tgl', function ($row) {
+                return $row->kibATanah->sertifikat_tgl
+                    ? Carbon::parse($row->kibATanah->sertifikat_tgl)->format('d-m-Y') // Format tanggal
+                    : '-';
+            })
+            ->addColumn('sertifikat_no', function ($row) {
+                return $row->kibATanah->sertifikat_no ?? '-';
+            })
+            ->addColumn('penggunaan', function ($row) {
+                return $row->kibATanah->penggunaan ?? '-';
+            })
+
+            ->addColumn('tanggal_perolehan', function ($row) {
+                return $row->tanggal_perolehan
+                    ? date('d-M-Y', strtotime($row->tanggal_perolehan))
+                    : '-';
             })
             ->addColumn('usia_aset', function ($row) {
                 $tanggalPerolehan = $row->tanggal_perolehan ? Carbon::parse($row->tanggal_perolehan) : null;
@@ -662,10 +700,216 @@ class AsetTetapController extends Controller
                 $usiaAsetBulan = $tanggalPerolehan ? now()->diffInMonths($tanggalPerolehan) % 12 : 0;
                 return "$usiaAsetTahun Tahun, $usiaAsetBulan Bulan";
             })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+    private function datawali2($query)
+    {
+        return DataTables::of($query)
+            ->addIndexColumn()
 
+            ->addColumn('nama_barang', function ($row) {
+                return $row->jenisAset->name . ' / ' . $row->nama_barang;
+            })
+
+            ->addColumn('merk', function ($row) {
+                return $row->KibBPeralatanmesin->merk_type ?? '-'; // Jika relasi kosong, tampilkan "-"
+            })
+            ->addColumn('ukuran', function ($row) {
+                return $row->KibBPeralatanmesin->ukuran_cc ?? '-';
+            })
+            ->addColumn('bahan', function ($row) {
+                return $row->KibBPeralatanmesin->bahan ?? '-';
+            })
+            ->addColumn('nomor_pabrik', function ($row) {
+                return $row->KibBPeralatanmesin->no_pabrik ?? '-';
+            })
+            ->addColumn('nomor_rangka', function ($row) {
+                return $row->KibBPeralatanmesin->no_rangka ?? '-';
+            })
+
+            ->addColumn('nomor_mesin', function ($row) {
+                return $row->KibBPeralatanmesin->no_mesin ?? '-';
+            })
+            ->addColumn('nomor_polisi', function ($row) {
+                return $row->KibBPeralatanmesin->no_polisi ?? '-';
+            })
+            ->addColumn('nomor_bpkb', function ($row) {
+                return $row->KibBPeralatanmesin->no_bpkb ?? '-';
+            })
+            ->addColumn('tahun_pengadaan', function ($row) {
+                return $row->KibBPeralatanmesin->tahun_pengadaan ?? '-';
+            })
+            ->addColumn('tanggal_perolehan', function ($row) {
+                return $row->tanggal_perolehan
+                    ? date('d-M-Y', strtotime($row->tanggal_perolehan))
+                    : '-';
+            })
+            ->addColumn('usia_aset', function ($row) {
+                $tanggalPerolehan = $row->tanggal_perolehan ? Carbon::parse($row->tanggal_perolehan) : null;
+                $usiaAsetTahun = $tanggalPerolehan ? now()->diffInYears($tanggalPerolehan) : 0;
+                $usiaAsetBulan = $tanggalPerolehan ? now()->diffInMonths($tanggalPerolehan) % 12 : 0;
+                return "$usiaAsetTahun Tahun, $usiaAsetBulan Bulan";
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+    private function datawali3($query)
+    {
+        return DataTables::of($query)
+            ->addIndexColumn()
+
+            ->addColumn('nama_barang', function ($row) {
+                return $row->jenisAset->name . ' / ' . $row->nama_barang;
+            })
+
+            ->addColumn('kondisi_bangunan', function ($row) {
+                return $row->KibCGedungbangunan->kondisi_bangunan ?? '-'; // Jika relasi kosong, tampilkan "-"
+            })
+            ->addColumn('bertingkat', function ($row) {
+                return $row->KibCGedungbangunan->bertingkat ?? '-';
+            })
+            ->addColumn('beton', function ($row) {
+                return $row->KibCGedungbangunan->beton ?? '-';
+            })
+            ->addColumn('luas_lantai', function ($row) {
+                return $row->KibCGedungbangunan->luas_lantai ?? '-';
+            })
+            ->addColumn('dokumen_tanggal', function ($row) {
+                return $row->KibCGedungbangunan->dok_tgl ?? '-';
+            })
+
+            ->addColumn('dokumen_nomor', function ($row) {
+                return $row->KibCGedungbangunan->dok_no ?? '-';
+            })
+            ->addColumn('luas', function ($row) {
+                return $row->KibCGedungbangunan->luas ?? '-';
+            })
+            ->addColumn('status_tanah', function ($row) {
+                return $row->KibCGedungbangunan->status_tanah ?? '-';
+            })
+            ->addColumn('nomor_kode_tanah', function ($row) {
+                return $row->KibCGedungbangunan->no_kode_tanah ?? '-';
+            })
+            ->addColumn('tanggal_perolehan', function ($row) {
+                return $row->tanggal_perolehan
+                    ? date('d-M-Y', strtotime($row->tanggal_perolehan))
+                    : '-';
+            })
+            ->addColumn('usia_aset', function ($row) {
+                $tanggalPerolehan = $row->tanggal_perolehan ? Carbon::parse($row->tanggal_perolehan) : null;
+                $usiaAsetTahun = $tanggalPerolehan ? now()->diffInYears($tanggalPerolehan) : 0;
+                $usiaAsetBulan = $tanggalPerolehan ? now()->diffInMonths($tanggalPerolehan) % 12 : 0;
+                return "$usiaAsetTahun Tahun, $usiaAsetBulan Bulan";
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
+    private function datawali4($query)
+    {
+        return DataTables::of($query)
+            ->addIndexColumn()
+
+            ->addColumn('nama_barang', function ($row) {
+                return $row->jenisAset->name . ' / ' . $row->nama_barang;
+            })
+
+            ->addColumn('konstruksi', function ($row) {
+                return $row->KibDJalanirigasi->konstruksi ?? '-'; // Jika relasi kosong, tampilkan "-"
+            })
+            ->addColumn('panjang', function ($row) {
+                return $row->KibDJalanirigasi->panjang ?? '-';
+            })
+            ->addColumn('lebar', function ($row) {
+                return $row->KibDJalanirigasi->lebar ?? '-';
+            })
+            ->addColumn('luas', function ($row) {
+                return $row->KibDJalanirigasi->luas ?? '-';
+            })
+            ->addColumn('alamat', function ($row) {
+                return $row->KibDJalanirigasi->alamat ?? '-';
+            })
+            ->addColumn('kondisi', function ($row) {
+                return $row->KibDJalanirigasi->kondisi ?? '-';
+            })
+            ->addColumn('dokumen_tanggal', function ($row) {
+                return $row->KibDJalanirigasi->dok_tgl ?? '-';
+            })
+
+            ->addColumn('dokumen_nomor', function ($row) {
+                return $row->KibDJalanirigasi->dok_no ?? '-';
+            })
+            ->addColumn('status_tanah', function ($row) {
+                return $row->KibDJalanirigasi->status_tanah ?? '-';
+            })
+            ->addColumn('nomor_kode_tanah', function ($row) {
+                return $row->KibDJalanirigasi->no_kode_tanah ?? '-';
+            })
+
+            ->addColumn('tanggal_perolehan', function ($row) {
+                return $row->tanggal_perolehan
+                    ? date('d-M-Y', strtotime($row->tanggal_perolehan))
+                    : '-';
+            })
+            ->addColumn('usia_aset', function ($row) {
+                $tanggalPerolehan = $row->tanggal_perolehan ? Carbon::parse($row->tanggal_perolehan) : null;
+                $usiaAsetTahun = $tanggalPerolehan ? now()->diffInYears($tanggalPerolehan) : 0;
+                $usiaAsetBulan = $tanggalPerolehan ? now()->diffInMonths($tanggalPerolehan) % 12 : 0;
+                return "$usiaAsetTahun Tahun, $usiaAsetBulan Bulan";
+            })
+            ->rawColumns(['aksi'])
             ->make(true);
     }
 
+    private function datawali5($query)
+    {
+        return DataTables::of($query)
+            ->addIndexColumn()
+
+            ->addColumn('nama_barang', function ($row) {
+                return $row->jenisAset->name . ' / ' . $row->nama_barang;
+            })
+
+            ->addColumn('judul', function ($row) {
+                return $row->KibEAsetlainnya->judul ?? '-'; // Jika relasi kosong, tampilkan "-"
+            })
+            ->addColumn('pencipta', function ($row) {
+                return $row->KibEAsetlainnya->pencipta ?? '-';
+            })
+            ->addColumn('spesifikasi', function ($row) {
+                return $row->KibEAsetlainnya->spesifikasi ?? '-';
+            })
+            ->addColumn('asal_daerah', function ($row) {
+                return $row->KibEAsetlainnya->asal_daerah ?? '-';
+            })
+            ->addColumn('bahan', function ($row) {
+                return $row->KibEAsetlainnya->bahan ?? '-';
+            })
+            ->addColumn('jenis', function ($row) {
+                return $row->KibEAsetlainnya->jenis ?? '-';
+            })
+            ->addColumn('ukuran', function ($row) {
+                return $row->KibEAsetlainnya->ukuran ?? '-';
+            })
+
+            ->addColumn('jumlah', function ($row) {
+                return $row->KibEAsetlainnya->jumlah ?? '-';
+            })
+
+            ->addColumn('tanggal_perolehan', function ($row) {
+                return $row->tanggal_perolehan
+                    ? date('d-M-Y', strtotime($row->tanggal_perolehan))
+                    : '-';
+            })
+            ->addColumn('usia_aset', function ($row) {
+                $tanggalPerolehan = $row->tanggal_perolehan ? Carbon::parse($row->tanggal_perolehan) : null;
+                $usiaAsetTahun = $tanggalPerolehan ? now()->diffInYears($tanggalPerolehan) : 0;
+                $usiaAsetBulan = $tanggalPerolehan ? now()->diffInMonths($tanggalPerolehan) % 12 : 0;
+                return "$usiaAsetTahun Tahun, $usiaAsetBulan Bulan";
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
     public function edit($id)
     {
 
---
+++ resources/views/penghapusan-aset/index-wali/_kib_e.blade.php
@@ -0,0 +1,81 @@
+<div class="tab-pane {{ $key === 0 ? 'active' : '' }}" id="tab{{ $key }}">
+    <div class="box">
+        <div class="box-header">
+
+            <h4> Data {{ $c->name }}</h4>
+        </div>
+        <div class="box-header">
+   
+            <a href="{{ route('penghapusanasetwali.print', ['id' => $c->id, 'year' => request('year') ?? $year]) }}"
+                target="_blank" id="btnPrintPDF{{ $key }}" class="btn btn-primary">
+                <i class="fa fa-file-pdf-o"></i> Print
+            </a>
+
+
+
+        </div>
+        <div class="box-body">
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
+            <div class="table-responsive">
+                <div class="mb-3 ">
+                    <label for="filterYear{{ $key }}">Filter Tahun:</label>
+                    <select id="filterYear{{ $key }}" class="form-control">
+                        <option value="">Semua Tahun</option>
+                        @foreach ($years as $year)
+                            <option value="{{ $year }}">{{ $year }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <br>
+                <table id="example{{ $key }}" class="table table-bordered table-striped" style="width: 100%;">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                     
+                            <th>Jenis Barang / Nama Barang</th>
+                            <th>No. Kode</th>
+                            <th>No. Register</th>
+                            <th>Judul</th>
+                            <th>Pencipta</th>
+                            <th>Spesifikasi</th>
+                            <th>Asal Daerah</th>
+                            <th>Bahan</th>
+                            <th>Jenis</th>
+                            <th>Ukuran</th>
+                            <th>Jumlah</th>
+                            <th>Asal Usul</th>
+                            <th>Harga</th>
+                            <th>Tanggal Perolehan Aset</th>
+                            <th>Usia Aset</th>
+                            <th>Keterangan</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="createModal{{ $key }}" tabindex="-1" role="dialog"
+            aria-labelledby="createModalLabel{{ $key }}" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="createModalLabel{{ $key }}">
+                            Tambah
+                            Data {{ $c->name }}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body" id="CreateFormContainer{{ $key }}">
+                        <p class="text-center">Loading...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
